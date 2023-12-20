9e8d58ca38237972213ace4c7d552ed09308d788
Alsaad <mrrgx.22@gmail.com>
2023-12-20 11:34:40 +0300

    Initial Commit

---
+++ moha/mohaApp.swift
@@ -0,0 +1,17 @@
+//
+//  mohaApp.swift
+//  moha
+//
+//  Created by Mohammed on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct mohaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
